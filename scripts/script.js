16b22737c2e9b972a66d0ef54ab5d153d2abd677
2001Marty <martin.prasil.2001@gmail.com>
2022-01-08 15:49:33 +0100

    processing data from opeanweathermap api to html

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 const search = document.getElementById('search');
 const match_list = document.getElementById('match-list');
 const city_name = document.getElementById('city-name');
+const weather_stats = document.getElementById('weather-stats');
 
 const APIkey = '237acc5a5da622a10304477e0a781034';
+const days = ['Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Neděle'];
+
+
+
 
 
 var active_city = {
@@ -12,12 +17,33 @@ var active_city = {
 };
 
 const fetchWeather = async (lat, lon) => {
-  //  const URL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${active_city}&cnt=5&appid=${APIkey}`;
-    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude={part,hourly,minutely}&appid=${APIkey}`;
+    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude={part,hourly,minutely}&lang=cz&appid=${APIkey}`;
     const res = await fetch(URL);
     const data = await res.json();
     console.log(data);
+    outputWeatherHtml(data);
 };
+
+const outputWeatherHtml = data => {
+    const html = data.daily.map((day, id) =>{
+        if(id <= 4){
+            let dt = new Date(day.dt * 1000);
+            return `
+            <div class="day-card-container">
+                <h2 class="day-name">${days[dt.getDay()]} <small>${dt.toLocaleDateString()}</small></h4>
+                <img class="weather-icon" src=" http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png"  alt="${day.weather[0].description}">
+                <p class="desc">${capitalizeFirstLetter(day.weather[0].description)}</p>
+                <p class="max-temp">Denní teplota: ${Math.floor(day.temp.day)} °C</p>
+                <p class="min-temp">Noční tepolota: ${Math.floor(day.temp.night)} °C</p>
+                <p class="feels-like-temp">Pocitová teplota: ${Math.floor(day.feels_like.day)} °C</p>
+                <p class="wind-speed">Rychlost větru: ${Math.floor(day.wind_speed)} km/h</p>
+            </div>
+       `
+        }
+        
+    }).join('');
+    weather_stats.innerHTML= html;
+}
 
 const searchCities = async searchText => {
     const res = await fetch('./data/city.list.json');
@@ -54,6 +80,11 @@ const outputHtml = matches => {
        
     }
 }
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
 
 function setCity(name, lat, lon) {
     active_city.name = name;
