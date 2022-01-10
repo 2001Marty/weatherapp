3496db6bd65474c38e2af65a1af89e8b12785526
2001Marty <martin.prasil.2001@gmail.com>
2022-01-11 00:26:02 +0100

    city name from geolocation

--- scripts/location.js
+++ scripts/location.js
@@ -1,15 +1,33 @@
-const successCallback = (position) => {
-   setCity('Tvá lokace', position.coords.latitude, position.coords.longitude);
-   console.log(position);
-   
-
+const successCallback = async(position) => {
+  getCityName(position);
+  setCity(await getCityName(position), position.coords.latitude, position.coords.longitude);
 };
 
 const errorCallback = (error) => {
-    console.log(error)
+  console.log(error)
 };
 
+var options = {
+  nableHighAccuracy: true,
+  timeout: 5000,
+};
 
+const getCityName = async (position) =>{
+  const res = await fetch('./data/city.list.json');
+  const cities = await res.json();
+  const city_name = cities.filter(city => 
+    city.coord.lat.toFixed(1) == position.coords.latitude.toFixed(1) &&
+    city.coord.lon.toFixed(1) == position.coords.longitude.toFixed(1)
+    );
+  if(city_name.length != 0){
+    const indexArr = city_name.map((c) => {
+      return Math.abs(c.coord.lat - position.coords.latitude);
+    });
+    const min = Math.min(...indexArr)
+    const closest = city_name[indexArr.indexOf(min)]
+    return closest.name
+  }else return 'tvá lokace';
+};
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
 
